3425c5f50607184fcccd02cb1210b93d96a1c841
AtanasKaloyanov <atanas.kaloyanov@yahoo.com>
2022-04-04 23:48:16 +0300

    I'm adding the first theme from the cource Programming Fundamentals with Java.

--- T01StackAndQueues/Lab/P01BrowserHistory.java
+++ T01StackAndQueues/Lab/P01BrowserHistory.java
@@ -1,4 +1,9 @@
 package T01StackAndQueues.Lab;
 
+import java.util.Scanner;
+
 public class P01BrowserHistory {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
 }
