aa5f750fe6d9fa4715c0319a5ded27fea70b8216
AtanasKaloyanov <atanas.kaloyanov@yahoo.com>
2022-04-05 23:49:08 +0300

    I'm adding the solution of the task Browser History.

--- T01StackAndQueues/Lab/P01BrowserHistory.java
+++ T01StackAndQueues/Lab/P01BrowserHistory.java
@@ -1,9 +1,28 @@
 package T01StackAndQueues.Lab;
 
+import java.util.ArrayDeque;
 import java.util.Scanner;
 
 public class P01BrowserHistory {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        ArrayDeque<String> browser = new ArrayDeque<>();
+        String line = scanner.nextLine();
+
+        String current = "";
+        while(!line.equals("Home")) {
+            if(line.equals("back")) {
+                if(!browser.isEmpty()) { current = browser.pop();
+                } else {
+                    System.out.println("no previous URLs");
+                    line = scanner.nextLine();
+                    continue; }
+            } else {
+                if(!current.equals("")) { browser.push(current); }
+                current = line; }
+            System.out.println(current);
+            line = scanner.nextLine(); }
+
     }
 }
