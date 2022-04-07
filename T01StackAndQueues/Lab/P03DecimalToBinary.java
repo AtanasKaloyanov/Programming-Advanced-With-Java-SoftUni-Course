a6fc8d64eac3800dc58dac7938d8ffe84a9dee82
AtanasKaloyanov <atanas.kaloyanov@yahoo.com>
2022-04-07 22:36:23 +0300

    I'm adding the solution of the problem Decimal To Binary.

--- T01StackAndQueues/Lab/P03DecimalToBinary.java
+++ T01StackAndQueues/Lab/P03DecimalToBinary.java
@@ -1,4 +1,23 @@
 package T01StackAndQueues.Lab;
 
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
 public class P03DecimalToBinary {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int decimal = Integer.valueOf(scanner.nextLine());
+
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+
+
+        while (decimal != 0)
+            stack.push(decimal % 2);
+        decimal /= 2;
+
+        while (!stack.isEmpty())
+            System.out.print(stack.pop());
+
+    }
 }
