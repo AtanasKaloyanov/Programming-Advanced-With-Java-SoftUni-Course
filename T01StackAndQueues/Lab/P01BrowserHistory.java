207197e719b15992bd84bbe731ca5dd8a6a3d2f3
AtanasKaloyanov <atanas.kaloyanov@yahoo.com>
2022-04-18 00:36:08 +0300

    I'm adding the solution of the task Browser History.

--- T01StackAndQueues/Lab/P01BrowserHistory.java
+++ T01StackAndQueues/Lab/P01BrowserHistory.java
@@ -6,23 +6,42 @@ import java.util.Scanner;
 public class P01BrowserHistory {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        ArrayDeque<String> stack = new ArrayDeque<>();
 
-        ArrayDeque<String> browser = new ArrayDeque<>();
-        String line = scanner.nextLine();
-
-        String current = "";
-        while(!line.equals("Home")) {
-            if(line.equals("back")) {
-                if(!browser.isEmpty()) { current = browser.pop();
-                } else {
-                    System.out.println("no previous URLs");
-                    line = scanner.nextLine();
-                    continue; }
+        while (!input.equals("Home")) {
+            if (!input.equals("back")) {
+                stack.push(input);
+                System.out.println(stack.peek());
             } else {
-                if(!current.equals("")) { browser.push(current); }
-                current = line; }
-            System.out.println(current);
-            line = scanner.nextLine(); }
+                if (stack.size() == 1 || stack.size() == 0) {
+                    System.out.println("no previous URLs");
+                } else {
+                    stack.pop();
+                    System.out.println(stack.peek());
+                }
+            }
+            input = scanner.nextLine();
+        }
+
+        //https//softuni.bg/
+        //back
+        //https//softuni.bg/trainings/courses
+        //back
+        //https//softuni.bg/trainings/2056
+        //back
+        //https//softuni.bg/trainings/live
+        //https//softuni.bg/trainings/live/details
+        //Home
+
+        //https//softuni.bg/
+        //no previous URLs
+        //https//softuni.bg/trainings/courses
+        //https//softuni.bg/
+        //https//softuni.bg/trainings/2056
+        //https//softuni.bg/
+        //https//softuni.bg/trainings/live
+        //https//softuni.bg/trainings/live/details
 
     }
 }
