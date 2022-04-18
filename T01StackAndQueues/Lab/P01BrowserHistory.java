9acef0f37f2ee35b8739a1f7d767939bed8ac46d
AtanasKaloyanov <atanas.kaloyanov@yahoo.com>
2022-04-18 21:22:34 +0300

    I'm adding a modification of the task Browser History.

--- T01StackAndQueues/Lab/P01BrowserHistory.java
+++ T01StackAndQueues/Lab/P01BrowserHistory.java
@@ -6,20 +6,21 @@ import java.util.Scanner;
 public class P01BrowserHistory {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         String input = scanner.nextLine();
         ArrayDeque<String> stack = new ArrayDeque<>();
 
         while (!input.equals("Home")) {
-            if (!input.equals("back")) {
-                stack.push(input);
-                System.out.println(stack.peek());
-            } else {
-                if (stack.size() == 1 || stack.size() == 0) {
+            if (input.equals("back")) {
+                if (stack.size() <= 1) {
                     System.out.println("no previous URLs");
                 } else {
                     stack.pop();
                     System.out.println(stack.peek());
                 }
+            } else {
+                stack.push(input);
+                System.out.println(stack.peek());
             }
             input = scanner.nextLine();
         }
