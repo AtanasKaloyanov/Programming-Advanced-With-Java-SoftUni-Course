79ce1432cfcb7c26b3a3e44362c55bdc90d58e12
AtanasKaloyanov <atanas.kaloyanov@yahoo.com>
2022-04-22 18:29:51 +0300

    This a different solution of the problem Matching Brackets.

--- T01StackAndQueues/Lab/P04MatchingBrackets.java
+++ T01StackAndQueues/Lab/P04MatchingBrackets.java
@@ -1,25 +1,31 @@
 package T01StackAndQueues.Lab;
 
 import java.util.ArrayDeque;
-import java.util.Deque;
 import java.util.Scanner;
 
 public class P04MatchingBrackets {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
 
-        Deque<Integer> stack = new ArrayDeque<>();
         for (int i = 0; i < input.length(); i++) {
-            char currentChar = input.charAt(i);
-            if (currentChar == '(') {
+            char currenChar = input.charAt(i);
+            if (currenChar == '(') {
                 stack.push(i);
-            } else if (currentChar == ')') {
-                int startIndex = stack.peek();
+            } else if (currenChar == ')'){
+                int openBracketPosition = stack.peek();
+                int closingBracketPosition = i;
+                String expressionWithBrackets = input.substring(openBracketPosition, closingBracketPosition + 1);
+                System.out.println(expressionWithBrackets);
                 stack.pop();
-                String expressionInBrackets = input.substring(startIndex, i + 1);
-                System.out.println(expressionInBrackets);
             }
         }
     }
 }
+
+//   1 + (2 - (2 + 3) * 4 / (3 + 1)) * 5
+
+//  (2 + 3)
+//  (3 + 1)
+//  (2 - (2 + 3) * 4 / (3 + 1))
