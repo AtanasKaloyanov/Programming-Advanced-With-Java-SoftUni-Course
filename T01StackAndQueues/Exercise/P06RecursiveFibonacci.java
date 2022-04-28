4bf1a61c6dd1e6e9592d86d4fb9ce7d2afbdcf0a
AtanasKaloyanov <atanas.kaloyanov@yahoo.com>
2022-04-28 16:26:09 +0300

    I'm adding the solution of the problem Recursive Fibonacci.

---
+++ T01StackAndQueues/Exercise/P06RecursiveFibonacci.java
@@ -0,0 +1,25 @@
+package T01StackAndQueues.Exercise;
+
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class P06RecursiveFibonacci {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int number = Integer.parseInt(scanner.nextLine());
+        ArrayDeque<Long> stack = new ArrayDeque<>();
+        stack.push(1L);
+        stack.push(1L);
+
+        for (int i = 1; i < number ; i++) {
+            long a = stack.pop();
+            long b = stack.pop();
+            long c = a + b;
+            stack.push(b);
+            stack.push(a);
+            stack.push(c);
+        }
+        System.out.println(stack.peek());
+    }
+}
