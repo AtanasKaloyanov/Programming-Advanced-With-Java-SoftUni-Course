5cd68dabdcef0a18c58f6821a89a3fea87b422c1
AtanasKaloyanov <atanas.kaloyanov@yahoo.com>
2022-04-30 00:32:49 +0300

    I'm adding the solution of the problem Sum Matrix Element.

---
+++ T02MultidimensionalArrays/Lab/P04SumMatrixElements.java
@@ -0,0 +1,26 @@
+package T02MultidimensionalArrays.Lab;
+
+import java.util.Scanner;
+
+public class P04SumMatrixElements {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] inputArray = scanner.nextLine().split(", ");
+        int rows = Integer.parseInt(inputArray[0]);
+        int columns = Integer.parseInt(inputArray[1]);
+
+        int counter = 0;
+
+        for (int i = 0; i < rows; i++) {
+                String[] array = scanner.nextLine().split(", ");
+            for (int j = 0; j < columns; j++) {
+                int currentElement = Integer.parseInt(array[j]);
+                counter += currentElement;
+            }
+        }
+        System.out.println(rows);
+        System.out.println(columns);
+        System.out.println(counter);
+    }
+}
