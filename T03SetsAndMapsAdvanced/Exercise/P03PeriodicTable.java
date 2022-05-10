a090532940edc5af0022658c277fd69a2ef2b4ec
AtanasKaloyanov <atanas.kaloyanov@yahoo.com>
2022-05-10 23:19:51 +0300

    This is the solution of the problem "Periodic Table".

---
+++ T03SetsAndMapsAdvanced/Exercise/P03PeriodicTable.java
@@ -0,0 +1,23 @@
+package T03SetsAndMapsAdvanced.Exercise;
+
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class P03PeriodicTable {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int number = Integer.parseInt(scanner.nextLine());
+        Set<String> set = new TreeSet<>();
+
+        for (int i = 1; i <= number ; i++) {
+            String[] array = scanner.nextLine().split(" ");
+            for (int j = 0; j < array.length; j++) {
+                set.add(array[j]);
+            }
+        }
+
+        System.out.println(String.join(" ", set));
+    }
+}
