8bd8559474e01b32fe4bb944dc59da0c25a1b20b
AtanasKaloyanov <atanas.kaloyanov@yahoo.com>
2022-05-11 17:21:04 +0300

    This is the final solution of the problem "Hands Of Cards".

--- T03SetsAndMapsAdvanced/Exercise/P08HandsOfCard.java
+++ T03SetsAndMapsAdvanced/Exercise/P08HandsOfCard.java
@@ -39,60 +39,7 @@ public class P08HandsOfCard {
                 int typerInt = 0;
 
                 char power = currentCard.charAt(0);
-
-                switch (power) {
-                    case '2':
-                        powerInt = 2;
-                        break;
-
-                    case '3':
-                        powerInt = 3;
-                        break;
-
-                    case '4':
-                        powerInt = 4;
-                        break;
-
-                    case '5':
-                        powerInt = 5;
-                        break;
-
-                    case '6':
-                        powerInt = 6;
-                        break;
-
-                    case '7':
-                        powerInt = 7;
-                        break;
-
-                    case '8':
-                        powerInt = 8;
-                        break;
-
-                    case '9':
-                        powerInt = 9;
-                        break;
-
-                    case '1':
-                        powerInt = 10;
-                        break;
-
-                    case 'J':
-                        powerInt = 11;
-                        break;
-
-                    case 'Q':
-                        powerInt = 12;
-                        break;
-
-                    case 'K':
-                        powerInt = 13;
-                        break;
-
-                    case 'A':
-                        powerInt = 14;
-                        break;
-                }
+                powerInt = getPowerInt(power, powerInt);
 
                 char type;
 
@@ -102,23 +49,7 @@ public class P08HandsOfCard {
                     type = currentCard.charAt(2);
                 }
 
-                switch (type) {
-                    case 'S':
-                        typerInt = 4;
-                        break;
-
-                    case 'H':
-                        typerInt = 3;
-                        break;
-
-                    case 'D':
-                        typerInt = 2;
-                        break;
-
-                    case 'C':
-                        typerInt = 1;
-                        break;
-                }
+                typerInt =  getTypeInt(type, typerInt);
 
                 int currentSum = powerInt * typerInt;
                 sum += currentSum;
@@ -127,4 +58,83 @@ public class P08HandsOfCard {
             System.out.printf("%d%n", sum);
         });
     }
+
+    private static int getTypeInt(char type, int typerInt) {
+        switch (type) {
+            case 'S':
+                typerInt = 4;
+                break;
+
+            case 'H':
+                typerInt = 3;
+                break;
+
+            case 'D':
+                typerInt = 2;
+                break;
+
+            case 'C':
+                typerInt = 1;
+                break;
+        }
+        return typerInt;
+    }
+
+    private static int getPowerInt(char power, int powerInt) {
+        switch (power) {
+            case '2':
+                powerInt = 2;
+                break;
+
+            case '3':
+                powerInt = 3;
+                break;
+
+            case '4':
+                powerInt = 4;
+                break;
+
+            case '5':
+                powerInt = 5;
+                break;
+
+            case '6':
+                powerInt = 6;
+                break;
+
+            case '7':
+                powerInt = 7;
+                break;
+
+            case '8':
+                powerInt = 8;
+                break;
+
+            case '9':
+                powerInt = 9;
+                break;
+
+            case '1':
+                powerInt = 10;
+                break;
+
+            case 'J':
+                powerInt = 11;
+                break;
+
+            case 'Q':
+                powerInt = 12;
+                break;
+
+            case 'K':
+                powerInt = 13;
+                break;
+
+            case 'A':
+                powerInt = 14;
+                break;
+        }
+        return powerInt;
+    }
+
 }
