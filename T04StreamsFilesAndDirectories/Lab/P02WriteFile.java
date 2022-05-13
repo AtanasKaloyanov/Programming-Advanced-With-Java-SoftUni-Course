050d82cdaad4aa4edce8f96b74e67cbef829b116
AtanasKaloyanov <atanas.kaloyanov@yahoo.com>
2022-05-13 23:17:48 +0300

    This is the final solution of the problem "Write File".

--- T04StreamsFilesAndDirectories/Lab/P02WriteFile.java
+++ T04StreamsFilesAndDirectories/Lab/P02WriteFile.java
@@ -1,34 +1,36 @@
 package T04StreamsFilesAndDirectories.Lab;
 
+
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 public class P02WriteFile {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
         String path = "D:\\Programming\\SoftUni\\Programming Advanced with Java\\9. Streams, Files and Directories\\Files Tor the Tasks\\input.txt";
-        FileInputStream fileStream = new FileInputStream(path);
+        String secondPath = "D:\\Programming\\SoftUni\\Programming Advanced with Java\\9. Streams, Files and Directories\\Files Tor the Tasks\\output.txt";
+
+        Set<Character> symbolsForDeleting = new HashSet<>();
+        Collections.addAll(symbolsForDeleting, '?', '!', ',', '.');
 
         try {
-            int oneByte = fileStream.read();
+            FileInputStream in = new FileInputStream(path);
+            FileOutputStream out = new FileOutputStream(secondPath);
 
+            int oneByte = in.read();
             while (oneByte >= 0) {
-
-                if (oneByte == 33 || oneByte == 44 || oneByte == 46 || oneByte == 63) {
-                    oneByte = fileStream.read();
-                    continue;
-                } else {
-                    System.out.print((char) oneByte);
+                char currentChar = (char) oneByte;
+                if (!symbolsForDeleting.contains(currentChar)) {
+                    out.write(currentChar);
                 }
-
-                oneByte = fileStream.read();
+                oneByte = in.read();
             }
-        }
-
-        catch (IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 }
