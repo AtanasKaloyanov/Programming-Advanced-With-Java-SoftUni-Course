eb035f62fbf47dd8f747dce04602f09e16498397
AtanasKaloyanov <atanas.kaloyanov@yahoo.com>
2022-05-14 00:30:09 +0300

    I'm adding the solution of the problem "Copy Bytes".

---
+++ T04StreamsFilesAndDirectories/Lab/P03CopyBytes.java
@@ -0,0 +1,35 @@
+package T04StreamsFilesAndDirectories.Lab;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class P03CopyBytes {
+    public static void main(String[] args) {
+
+        String inputPath = "D:\\Programming\\SoftUni\\Programming Advanced with Java\\9. Streams, Files and Directories\\Files Tor the Tasks\\input.txt";
+        String outputPath = "D:\\Programming\\SoftUni\\Programming Advanced with Java\\9. Streams, Files and Directories\\Files Tor the Tasks\\output.txt";
+
+        try {
+            FileInputStream in = new FileInputStream(inputPath);
+            FileOutputStream out = new FileOutputStream(outputPath);
+
+            int oneByte = in.read();
+
+            while (oneByte >= 0) {
+                if (oneByte == 32 || oneByte == 10) {
+                    out.write(oneByte);
+                } else {
+                    String digit = String.valueOf(oneByte);
+                    for (int i = 0; i < digit.length(); i++) {
+                        out.write(digit.charAt(i));
+                    }
+                }
+                oneByte = in.read();
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
