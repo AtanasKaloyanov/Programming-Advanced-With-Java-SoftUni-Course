79fb4dd11d342dc67ec50c741a0232a651dfd692
AtanasKaloyanov <atanas.kaloyanov@yahoo.com>
2022-05-16 15:22:41 +0300

    I'm adding the solution of the problem "Merge Two Files".

---
+++ T04StreamsFilesAndDirectories/Exercise/P07MergeTwoFiles.java
@@ -0,0 +1,55 @@
+package T04StreamsFilesAndDirectories.Exercise;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class P07MergeTwoFiles {
+    public static void main(String[] args) {
+
+        String firstPath = "D:\\Programming\\SoftUni\\Programming Advanced with Java\\10. Exercise - Streams, Files and Directories\\04. Java-Advanced-Files-and-Streams-Exercises-Resources\\inputOne.txt";
+        String secondPath = "D:\\Programming\\SoftUni\\Programming Advanced with Java\\10. Exercise - Streams, Files and Directories\\04. Java-Advanced-Files-and-Streams-Exercises-Resources\\inputTwo.txt";
+
+        try {
+            FileReader fileReader = new FileReader(firstPath);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            List<Integer> list = new ArrayList<>();
+
+            String line = bufferedReader.readLine();
+            while (line != null) {
+                String[] array = line.split("");
+                int currentNumber = Integer.parseInt(array[0]);
+                list.add(currentNumber);
+
+                line = bufferedReader.readLine();
+            }
+
+            FileReader secondFileReader = new FileReader(secondPath);
+            BufferedReader secondBufferedReader = new BufferedReader(secondFileReader);
+
+            String secondLine = secondBufferedReader.readLine();
+            while (secondLine != null) {
+                String[] array = secondLine.split(" ");
+                int currentNumber = Integer.parseInt(array[0]);
+                list.add(currentNumber);
+
+                secondLine = secondBufferedReader.readLine();
+            }
+
+
+            list.stream().sorted( (firstElement, secondElement) -> {
+                return firstElement.compareTo(secondElement);
+            }).forEach(number -> System.out.println(number));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
