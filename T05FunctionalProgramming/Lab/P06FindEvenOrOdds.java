82e8764ecae45d6d6bb34f73d43f1afa3c6b22dd
AtanasKaloyanov <atanas.kaloyanov@yahoo.com>
2022-05-18 00:48:05 +0300

    This is the solution of the problem "Find Even Or Odds".

---
+++ T05FunctionalProgramming/Lab/P06FindEvenOrOdds.java
@@ -0,0 +1,33 @@
+package T05FunctionalProgramming.Lab;
+
+import java.util.Scanner;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+import java.util.stream.IntStream;
+
+public class P06FindEvenOrOdds {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] array = scanner.nextLine().split(" ");
+        int begin = Integer.parseInt(array[0]);
+        int end = Integer.parseInt(array[1]);
+        String numberType = scanner.nextLine();
+
+        Predicate<Integer> predicate;
+
+        if (numberType.equals("even")) {
+            predicate = number -> number % 2 == 0;
+        } else {
+            predicate = number -> number % 2 != 0;
+        }
+
+        Consumer<Integer> printer = x -> System.out.print(x + " ");
+
+        IntStream
+                .rangeClosed(begin, end)
+                .filter(element -> predicate.test(element))
+                .boxed()
+                .forEach(printer);
+    }
+}
