68adee1f0db1dc80fbf391fa95c9400b6907fe3a
AtanasKaloyanov <atanas.kaloyanov@yahoo.com>
2022-05-23 19:12:39 +0300

    This is the solution of the problem "Opinion Poll".

---
+++ T06DifiningClasses/Exercise/Person.java
@@ -0,0 +1,19 @@
+package T06DifiningClasses.Exercise.P01OpinionPoll;
+
+public class Person {
+    String name;
+    int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
