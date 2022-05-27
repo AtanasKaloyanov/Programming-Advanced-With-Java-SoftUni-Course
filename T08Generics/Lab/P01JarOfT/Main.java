62fc1c349969feeb1a30eba56f71f95ac303d019
AtanasKaloyanov <atanas.kaloyanov@yahoo.com>
2022-05-27 17:41:06 +0300

    This is the final solution of the problem "Jar Of T"

--- T08Generics/Lab/P01JarOfT/Main.java
+++ T08Generics/Lab/P01JarOfT/Main.java
@@ -2,8 +2,17 @@ package T08Generics.Lab.P01JarOfT;
 
 public class Main {
     public static void main(String[] args) {
+
         Jar<Integer> jar = new Jar<>();
+        jar.add(1);
+        jar.add(2);
+        jar.add(3);
+        System.out.println(jar.remove());
 
         Jar<String> secondJar = new Jar<>();
+        secondJar.add("first");
+        secondJar.add("second");
+        secondJar.add("third");
+        System.out.println(secondJar.remove());
     }
 }
