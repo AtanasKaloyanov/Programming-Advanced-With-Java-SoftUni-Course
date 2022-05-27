a19ec14be6a5d77791d407582d7fdfd39a53446b
AtanasKaloyanov <atanas.kaloyanov@yahoo.com>
2022-05-27 16:48:54 +0300

    This is the solution of the problem "JarOfT"

---
+++ T08Generics/Lab/P01JarOfT/Jar.java
@@ -0,0 +1,20 @@
+package T08Generics.Lab.P01JarOfT;
+
+import java.util.ArrayDeque;
+
+public class Jar<T> {
+    private ArrayDeque<T> stack;
+
+    public Jar() {
+        this.stack = new ArrayDeque<>();
+    }
+
+    public void add(T element) {
+        this.stack.push(element);
+    }
+
+    public T remove() {
+       return this.stack.pop();
+    }
+
+}
---
+++ T08Generics/Lab/P01JarOfT/Main.java
@@ -0,0 +1,9 @@
+package T08Generics.Lab.P01JarOfT;
+
+public class Main {
+    public static void main(String[] args) {
+        Jar<Integer> jar = new Jar<>();
+
+        Jar<String> secondJar = new Jar<>();
+    }
+}
