92169e0ef9282364b336c236e5bc278905f9cf87
AtanasKaloyanov <atanas.kaloyanov@yahoo.com>
2022-06-02 00:37:02 +0300

    This is a exaple for Comparator.

---
+++ T09IteratorsAndComparators/Lab/PComparatorExample/Book.java
@@ -0,0 +1,40 @@
+package T09IteratorsAndComparators.Lab.PComparatorExample;
+
+import java.util.List;
+
+public class Book {
+    private String title;
+    private int year;
+    private List<String> authors;
+
+    public Book(String title, int year, String...authors) {
+        this.title = title;
+        this.year = year;
+        this.authors = List.of(authors);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+
+    public int getYear() {
+        return year;
+    }
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public List<String> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(List<String> authors) {
+        this.authors = authors;
+    }
+
+}
