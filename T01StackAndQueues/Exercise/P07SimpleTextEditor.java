8a50607851320641495563e7059bdf30fddb9055
AtanasKaloyanov <atanas.kaloyanov@yahoo.com>
2024-06-01 22:52:00 +0300

    This is the solution with some comments of the problem "Simple Text Editor".

--- T01StackAndQueues/Exercise/P07SimpleTextEditor.java
+++ T01StackAndQueues/Exercise/P07SimpleTextEditor.java
@@ -5,12 +5,14 @@ import java.util.Scanner;
 
 public class P07SimpleTextEditor {
     public static void main(String[] args) {
+        // 1. Input reading
         Scanner scanner = new Scanner(System.in);
-
         int numberCommands = Integer.parseInt(scanner.nextLine());
         StringBuilder sb = new StringBuilder();
-        ArrayDeque<String> stack = new ArrayDeque<>();
+        ArrayDeque<String[]> arrays = new ArrayDeque<>();
+        ArrayDeque<String> lastDeleted = new ArrayDeque<>();
 
+        // 2. Commands implementation
         for (int i = 1; i <= numberCommands; i++) {
             String[] array = scanner.nextLine().split(" ");
             String currentCommand = array[0];
@@ -18,25 +20,39 @@ public class P07SimpleTextEditor {
             switch (currentCommand) {
                 case "1":
                     String text = array[1];
-                    stack.push(sb.toString());
                     sb.append(text);
+                    arrays.push(array);
                     break;
 
                 case "2":
                     int count = Integer.parseInt(array[1]);
-                    stack.push(sb.toString());
+                    String deleted = sb.substring(sb.length() - count);
                     sb.delete(sb.length() - count, sb.length());
+                    arrays.push(array);
+                    lastDeleted.push(deleted);
                     break;
 
                 case "3":
                     int index = Integer.parseInt(array[1]);
-                    System.out.println(sb.charAt(index - 1));
+                    char lastElement = sb.charAt(index - 1);
+                    System.out.println(lastElement);
                     break;
 
                 case "4":
-                    sb = new StringBuilder(stack.pop());
+                    String[] lastArray = arrays.pop();
+                    String lastCommand = lastArray[0];
+
+                    if (lastCommand.equals("1")) {
+                        String lastAdded = lastArray[1];
+                        int lastAddedStartIndex = sb.toString().length() - lastAdded.length();
+                        sb.delete(lastAddedStartIndex, sb.length());
+                    } else if (lastCommand.equals("2")) {
+                        String lastErased = lastDeleted.pop();
+                        sb.append(lastErased);
+                    }
                     break;
             }
+
         }
     }
 }
