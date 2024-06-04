ab3b8f2a0cef34cb3a387b117bfeb924782e04e0
AtanasKaloyanov <atanas.kaloyanov@yahoo.com>
2024-06-05 00:39:23 +0300

    This is the updated solution of the problem "Positions of".

--- T02MultidimensionalArrays/Lab/P02PositionsOf.java
+++ T02MultidimensionalArrays/Lab/P02PositionsOf.java
@@ -5,40 +5,49 @@ import java.util.Scanner;
 
 public class P02PositionsOf {
     public static void main(String[] args) {
+        // 1. Input reading
         Scanner scanner = new Scanner(System.in);
+        // 1.1. Dimensions reading
+        int[] dimensions = readArray(scanner);
+        int rows = dimensions[0];
+        int columns = dimensions[1];
 
-        String[] array = scanner.nextLine().split(" ");
-        int rows = Integer.parseInt(array[0]);
-        int columns = Integer.parseInt(array[1]);
-
+        // 1.2. Matrix initializing and reading
         int[][] matrix = new int[rows][columns];
 
         for (int i = 0; i < rows; i++) {
-            int[] currentRow = Arrays.stream(scanner.nextLine().split(" "))
-                    .mapToInt(e -> Integer.parseInt(e))
-                    .toArray();
-
+            int[] currentRow = readArray(scanner);
             for (int j = 0; j < columns; j++) {
                 matrix[i][j] = currentRow[j];
             }
         }
 
+        // 1.3. Searching number reading
         int searchedNumber = Integer.parseInt(scanner.nextLine());
-        boolean find = false;
+        boolean isFound = false;
 
+        // 2. Nested cycles looping and by every match printing
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < columns; j++) {
                 int currentNumber = matrix[i][j];
 
-                if (searchedNumber == currentNumber) {
-                    find = true;
+                if (currentNumber == searchedNumber) {
+                    isFound = true;
                     System.out.println(i + " " + j);
                 }
             }
+
         }
 
-        if (!find) {
+        // 3. Final case printing by non-existing number
+        if (!isFound) {
             System.out.println("not found");
         }
+    }
+
+    private static int[] readArray(Scanner scanner) {
+        return Arrays.stream(scanner.nextLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
     }
 }
