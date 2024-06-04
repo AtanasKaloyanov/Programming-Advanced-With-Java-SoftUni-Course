78c056abee904cfba5670cb8d5a8c8a162a81f2b
AtanasKaloyanov <atanas.kaloyanov@yahoo.com>
2024-06-05 01:01:53 +0300

    This is the updated solution of the problem "Sum of the Matrix Elements".

--- T02MultidimensionalArrays/Lab/P04SumMatrixElements.java
+++ T02MultidimensionalArrays/Lab/P04SumMatrixElements.java
@@ -1,26 +1,33 @@
 package T02MultidimensionalArrays.Lab;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class P04SumMatrixElements {
     public static void main(String[] args) {
+        // 1. Dimensions reading and printing
         Scanner scanner = new Scanner(System.in);
-
-        String[] inputArray = scanner.nextLine().split(", ");
-        int rows = Integer.parseInt(inputArray[0]);
-        int columns = Integer.parseInt(inputArray[1]);
-
-        int sum = 0;
-
-        for (int i = 0; i < rows; i++) {
-                String[] array = scanner.nextLine().split(", ");
-            for (int j = 0; j < columns; j++) {
-                int currentElement = Integer.parseInt(array[j]);
-                sum += currentElement;
-            }
-        }
+        int[] dimensions = readArray(scanner);
+        int rows = dimensions[0];
+        int columns = dimensions[1];
         System.out.println(rows);
         System.out.println(columns);
-        System.out.println(sum);
+
+        // 2. Reading of arrays that are equal to the rows
+        int totalSum = 0;
+        for (int i = 0; i < rows; i++) {
+            int[] array = readArray(scanner);
+            int currentSum = Arrays.stream(array).sum();
+            totalSum += currentSum;
+        }
+
+        // 3. Printing of the total sum
+        System.out.println(totalSum);
+    }
+
+    private static int[] readArray(Scanner scanner) {
+        return Arrays.stream(scanner.nextLine().split(", "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
     }
 }
