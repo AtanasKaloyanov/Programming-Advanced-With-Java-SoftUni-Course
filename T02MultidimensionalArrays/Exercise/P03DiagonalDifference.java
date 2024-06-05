3d7d0bbf213f5cd9fcce3110f7b2365667f8c842
AtanasKaloyanov <atanas.kaloyanov@yahoo.com>
2024-06-06 00:09:44 +0300

    This is the updated solution of the problem "Diagonal's Difference".

--- T02MultidimensionalArrays/Exercise/P03DiagonalDifference.java
+++ T02MultidimensionalArrays/Exercise/P03DiagonalDifference.java
@@ -1,62 +1,45 @@
 package T02MultidimensionalArrays.Exercise;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class P03DiagonalDifference {
     public static void main(String[] args) {
+        // 1. Reading matrix
         Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+        int[][] matrix = new int[n][n];
 
-        int rowsAndColumns = Integer.parseInt(scanner.nextLine());
-        int[][] matrix = new int[rowsAndColumns][rowsAndColumns];
+        for (int i = 0; i < n; i++) {
+            int[] row = readRow(scanner);
+            matrix[i] = row;
+        }
 
-        fillingMatrix(matrix, scanner);
+        // 2. Finding the diagonal's sum
+        int primaryDiagSum = 0;
+        for (int j = 0; j < n; j++) {
+            int currentElement = matrix[j][j];
+            primaryDiagSum += currentElement;
+        }
 
-        int primaryDiagonalSum = getSumPrimaryDiagonal(matrix);
-        int secondaryDiagonalSum = getSumSecondaryDiagonal(matrix);
-        
-        int sum = Math.abs(primaryDiagonalSum - secondaryDiagonalSum);
-        System.out.println(sum);
+        int secondaryDiagSum = 0;
+        int i = n - 1;
+        for (int j = 0; j < n; j++) {
+            int currentElement = matrix[i][j];
+            secondaryDiagSum += currentElement;
+            i--;
+        }
+
+        // 3. Absolute value calculation and printing
+        int difference = Math.abs(primaryDiagSum - secondaryDiagSum);
+        System.out.println(difference);
     }
 
-    private static int getSumSecondaryDiagonal(int[][] matrix) {
-        int secondaryDiagonalSum = 0;
-        for (int row = 0; row < matrix.length; row++) {
-            secondaryDiagonalSum += matrix[row][matrix.length - row - 1];
-        }
-        return  secondaryDiagonalSum;
-    }
-
-    private static int getSumPrimaryDiagonal(int[][] matrix) {
-        int primaryDiagonalSum = 0;
-        for (int row = 0; row < matrix.length; row++) {
-            int currentElement = matrix[row][row];
-            primaryDiagonalSum += currentElement;
-        }
-        return primaryDiagonalSum;
-    }
-
-    private static void fillingMatrix(int[][] matrix, Scanner scanner) {
-        for (int row = 0; row < matrix.length; row++) {
-            String[] array = scanner.nextLine().split(" ");
-            for (int column = 0; column < matrix.length; column++) {
-                int currentElement = Integer.parseInt(array[column]);
-                matrix[row][column] = currentElement;
-            }
-        }
+    private static int[] readRow(Scanner scanner) {
+        return Arrays.stream(scanner.nextLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
     }
 }
 
-//         0   1  2
 
-// 0       11  2  4
-// 1       4   5  6
-// 2       10  8 -12
-
-
-// 00 - 11      01 - 2     02 - 4
-// 10 - 4       11 - 5     21 - 6
-// 20 - 10      21 - 8     22 - 12
-
-//primary diagonal - 00 - 11    11 - 5   22  - -12
-
-//secondary diagonal- 02 - 4    11 - 5   20  -  10
