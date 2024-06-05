f14c9f1610eb417bfad9ba66fe144c9c44a1fabd
AtanasKaloyanov <atanas.kaloyanov@yahoo.com>
2024-06-05 17:29:38 +0300

    This is the updated solution of the problem "Wrong Measurements".

--- T02MultidimensionalArrays/Lab/P08WrongMeasurements.java
+++ T02MultidimensionalArrays/Lab/P08WrongMeasurements.java
@@ -5,74 +5,78 @@ import java.util.Scanner;
 
 public class P08WrongMeasurements {
     public static void main(String[] args) {
+        // 1. Matrix reading
         Scanner scanner = new Scanner(System.in);
-
         int rows = Integer.parseInt(scanner.nextLine());
-        int columns = 0;
-        int[][] matrix = new int[rows][];
+        int[] row1 = readRow(scanner);
+        int columns = row1.length;
 
-        for (int i = 0; i < rows; i++) {
-            int[] array = Arrays.stream(scanner.nextLine().split(" "))
-                    .mapToInt(e -> Integer.parseInt(e))
-                    .toArray();
+        int[][] matrix = new int[rows][columns];
+        matrix[0] = row1;
 
-            columns = array.length;
-            matrix[i] = array;
-        }
+        matrixFilling(scanner, rows, columns, matrix);
 
-        String[] wrongElementData = scanner.nextLine().split(" ");
-        int wronElementRow = Integer.parseInt(wrongElementData[0]);
-        int wronElementColumn = Integer.parseInt(wrongElementData[1]);
-        int wrongElement = matrix[wronElementRow][wronElementColumn];
+        // 2. Finding the wrong element
+        int[] wrongDimensions = readRow(scanner);
+        int wrongRow = wrongDimensions[0];
+        int wrongColumn = wrongDimensions[1];
+        int wrongElement = matrix[wrongRow][wrongColumn];
 
-        int[][] finalMatrix = new int[rows][columns];
+        // 3. Creating a final matrix and filling it
+        int[][] resultMatrix = new int[rows][columns];
 
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < columns; j++) {
-                int currentElement = matrix[i][j];
-                if (currentElement == wrongElement) {
-                    int sum = 0;
+                int current = matrix[i][j];
+                if (current == wrongElement) {
+                    int left = 0;
+                    int up = 0;
+                    int right = 0;
+                    int down = 0;
 
-                    if (i > 0) {
-                        int upElement = matrix[i - 1][j];
-                        if (upElement != wrongElement) {
-                            sum += upElement;
-                        }
+                    if (j - 1 >= 0 && matrix[i][j - 1] != wrongElement) {
+                        left = matrix[i][j - 1];
+                    }
+                    if (i - 1 >= 0 && matrix[i - 1][j] != wrongElement) {
+                        up = matrix[i - 1][j];
+                    }
+                    if (j + 1 < columns && matrix[i][j + 1] != wrongElement) {
+                        right = matrix[i][j + 1];
+                    }
+                    if (i + 1 < rows && matrix[i + 1][j] != wrongElement) {
+                        down = matrix[i + 1][j];
                     }
 
-                    if (i < rows - 1) {
-                        int downElement = matrix[i + 1][j];
-                        if (downElement != wrongElement) {
-                            sum += downElement;
-                        }
-                    }
-
-                    if (j > 0) {
-                        int leftElement = matrix[i][j - 1];
-                        if (leftElement != wrongElement) {
-                            sum += leftElement;
-                        }
-                    }
-
-                    if (j < columns - 1) {
-                        int rigthElement = matrix[i][j + 1];
-                        if (rigthElement != wrongElement) {
-                            sum += rigthElement;
-                        }
-                    }
-
-                    finalMatrix[i][j] = sum;
+                    int sum = left + up + right + down;
+                    resultMatrix[i][j] = sum;
                 } else {
-                    finalMatrix[i][j] = matrix[i][j];
+                    resultMatrix[i][j] = current;
                 }
             }
         }
 
+        // 4. Output printing
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < columns; j++) {
-                System.out.print(finalMatrix[i][j] + " ");
+                System.out.print(resultMatrix[i][j] + " ");
             }
             System.out.println();
         }
+    }
+
+    private static void matrixFilling(Scanner scanner, int rows, int columns, int[][] matrix) {
+        for (int i = 1; i < rows; i++) {
+            int[] currentRow = readRow(scanner);
+            for (int j = 0; j < columns; j++) {
+                int currentElement = currentRow[j];
+                matrix[i][j] = currentElement;
+            }
+        }
+    }
+
+    private static int[] readRow(Scanner scanner) {
+        return Arrays.stream(scanner.nextLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
     }
 }
