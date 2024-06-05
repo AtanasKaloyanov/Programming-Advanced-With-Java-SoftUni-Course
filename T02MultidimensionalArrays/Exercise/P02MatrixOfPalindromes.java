512adac5c5447fbd2fde8fdfed2405cb21f1862c
AtanasKaloyanov <atanas.kaloyanov@yahoo.com>
2024-06-05 23:31:20 +0300

    This is the updated solution of the problem "Matrix of Palindromes".

--- T02MultidimensionalArrays/Exercise/P02MatrixOfPalindromes.java
+++ T02MultidimensionalArrays/Exercise/P02MatrixOfPalindromes.java
@@ -1,43 +1,30 @@
 package T02MultidimensionalArrays.Exercise;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class P02MatrixOfPalindromes {
     public static void main(String[] args) {
+        // 1. Rows and columns reading
         Scanner scanner = new Scanner(System.in);
+        int[] array = readDimensions(scanner);
+        int rows = array[0];
+        int columns = array[1];
 
-        String[] input = scanner.nextLine().split(" ");
-        int rows = Integer.parseInt(input[0]);
-        int columns = Integer.parseInt(input[1]);
-
-        String[][] matrix = new String[rows][columns];
-
-        fillMatrix(matrix, scanner, columns);
-
-        printMatrix(matrix, scanner, columns);
-    }
-
-    private static void printMatrix(String[][] matrix, Scanner scanner, int columns) {
-        for (int row = 0; row < matrix.length; row++) {
-            for (int column = 0; column < columns; column++) {
-                System.out.print(matrix[row][column] + " ");
+        // 2. Printing rows and column symbol
+        for (int i = 0; i < rows; i++) {
+            char rowSymbol = (char) (i + 97);
+            for (int j = 0; j < columns; j++) {
+                char columnSymbol = (char) (rowSymbol + j);
+                System.out.printf("%c%c%c ", rowSymbol, columnSymbol, rowSymbol);
             }
             System.out.println();
         }
     }
 
-    private static void fillMatrix(String[][] matrix, Scanner scanner, int columns) {
-        for (int row = 0; row < matrix.length; row++) {
-            for (int column = 0; column < columns; column++) {
-                char firstChar = (char) (97 + row);
-                char secondChar = (char) (97 + column + row);
-                char thirdChar = (char) (97 + row);
-                StringBuilder sb = new StringBuilder();
-                sb.append(firstChar);
-                sb.append(secondChar);
-                sb.append(thirdChar);
-                matrix[row][column] = sb.toString();
-            }
-        }
+    private static int[] readDimensions(Scanner scanner) {
+        return Arrays.stream(scanner.nextLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
     }
 }
