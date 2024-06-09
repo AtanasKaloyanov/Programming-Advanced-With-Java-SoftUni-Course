e9e0a4f86231efaed4dd1310ffd0aa66fa3a0480
AtanasKaloyanov <atanas.kaloyanov@yahoo.com>
2024-06-09 19:19:31 +0300

    This is the updated solution of the "Reverse Matrix Diagonals".

--- T02MultidimensionalArrays/Exercise/P11ReverseMatrixDiagonals.java
+++ T02MultidimensionalArrays/Exercise/P11ReverseMatrixDiagonals.java
@@ -5,49 +5,81 @@ import java.util.Scanner;
 
 public class P11ReverseMatrixDiagonals {
     public static void main(String[] args) {
-
+        // 1. Matrix reading
         Scanner scanner = new Scanner(System.in);
+        int[] coordinates = readRow(scanner);
+        int rows = coordinates[0];
+        int columns = coordinates[1];
 
-        String[] dimensions = scanner.nextLine().split(" ");
+        int[][] matrix = fillingMatrix(scanner, rows);
 
-        int rows = Integer.parseInt(dimensions[0]);
-        int columns = Integer.parseInt(dimensions[1]);
+        // 2. Printing diagonals
 
-        int[][] matrix = new int[rows][columns];
+        // 2.1. Initializing start rows and start columns
+        int startRow = rows - 1;
+        int startColumn = columns - 1;
 
-        fillMatrix(matrix,scanner);
+        // 2.2. Starting cycle until the start row is 0 or greater
+        while (startRow >= 0) {
+            // 2.3. Initializing print row and print column
+            int printRow = startRow;
+            int printColumn = startColumn;
 
-        int maxRowIndex = rows - 1;
-        int maxColumnIndex = columns - 1;
+            // 2.4 diagonal printing until the row and column are in the matrix
+            while (printRow >= 0 && printColumn < columns) {
+                int currentElement = matrix[printRow][printColumn];
+                System.out.print(currentElement + " ");
 
-        while (maxRowIndex >= 0) {
-            int row = maxRowIndex;
-            int column = maxColumnIndex;
-            while (column < columns && row >= 0) {
-                System.out.print(matrix[row][column] + " ");
-                row--;
-                column++;
+                printRow--;
+                printColumn++;
             }
+
+            // 2.5. After the printing of the diagonal row is the new line printing
             System.out.println();
-            maxColumnIndex--;
 
-            if (maxColumnIndex == -1) {
-                maxColumnIndex = 0;
-                maxRowIndex--;
-            }
-        }
-
-    }
-
-    private static void fillMatrix(int[][] matrix, Scanner scanner) {
-        for (int row = 0; row < matrix.length; row++) {
-            String[] array = scanner.nextLine().split(" ");
-            for (int column = 0; column < matrix[row].length; column++) {
-                matrix[row][column] = Integer.parseInt(array[column]);
+            // 2.6. Updating the start column and the start row
+            startColumn--;
+            if (startColumn == -1) {
+                startColumn = 0;
+                startRow--;
             }
         }
     }
 
+    private static int[][] fillingMatrix(Scanner scanner, int rows) {
+        int[][] matrix = new int[rows][];
+        for (int i = 0; i < rows; i++) {
+            int[] currentRow = readRow(scanner);
+            matrix[i] = currentRow;
+        }
+        return matrix;
+    }
+
+    private static int[] readRow(Scanner scanner) {
+        return Arrays.stream(scanner.nextLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+    }
 }
+
+/*
+Input 1:
+
+      0  1  2  3
+
+0     21 20 18 15
+1     19 17 14 12
+2     16 13 11 10
+
+Output's element's coordinates
+
+      23
+      22 12
+      21 12 03
+      20 11 02
+      10 11
+      00
+ */
+
 
 
