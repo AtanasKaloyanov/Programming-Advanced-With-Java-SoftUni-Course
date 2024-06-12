7917d6e86c1fd33fdaf8bf2e3026099c118a1ada
AtanasKaloyanov <atanas.kaloyanov@yahoo.com>
2024-06-12 23:10:15 +0300

    This is the solution of the problem "Product Shop".

--- T03SetsAndMapsAdvanced/Lab/P06ProductShop.java
+++ T03SetsAndMapsAdvanced/Lab/P06ProductShop.java
@@ -7,48 +7,58 @@ import java.util.TreeMap;
 
 public class P06ProductShop {
     public static void main(String[] args) {
+        // 1. Adding the products into a map
+        Map<String, Map<String, Double>> priceByProductMapByShop = new TreeMap<>();
         Scanner scanner = new Scanner(System.in);
-
-        Map<String, Map<String, Double>> store = new TreeMap<>();
-
-        String input = scanner.nextLine();
-        while (!input.equals("Revision")) {
-
-            String[] array = input.split(", ");
-
-            String storeName = array[0];
+        String line = scanner.nextLine();
+        while (!line.equals("Revision")) {
+            String[] array = line.split(", ");
+            String shopName = array[0];
             String product = array[1];
             double price = Double.parseDouble(array[2]);
 
-            store.putIfAbsent(storeName, new LinkedHashMap<>());
-            store.get(storeName).put(product, price);
+            priceByProductMapByShop.putIfAbsent(shopName, new LinkedHashMap<>());
+            Map<String, Double> productByPrice = priceByProductMapByShop.get(shopName);
+            productByPrice.put(product, price);
 
-            input = scanner.nextLine();
+            priceByProductMapByShop.put(shopName, productByPrice);
+            line = scanner.nextLine();
         }
 
-        store.forEach( (key, value) -> {
-            System.out.printf("%s->%n", key);
-            value.forEach( (valueKey, valueValue) -> System.out.printf("Product: %s, Price: %.1f%n", valueKey, valueValue));
+        // 2. Output printing
+        priceByProductMapByShop.entrySet().forEach( (outerEntry) -> {
+            String shopName = outerEntry.getKey();
+            System.out.printf("%s->\n", shopName);
+            Map<String, Double> pricesByProducts = outerEntry.getValue();
+            pricesByProducts.entrySet().forEach( (innerEntry) -> {
+                String product = innerEntry.getKey();
+                double price = innerEntry.getValue();
+                System.out.printf("Product: %s, Price: %.1f\n", product, price);
+            });
         });
 
-
     }
-}// input:
+}
 
-//lidl, peach, 1.20
-// lidl, juice, 2.30
-//fantastico, apple, 1.20
-//kaufland, banana, 1.10
-//fantastico, grape, 2.20
-//Revision
+/*
+//Input 1:
 
-//output:
+lidl, peach, 1.20
+ lidl, juice, 2.30
+fantastico, apple, 1.20
+kaufland, banana, 1.10
+fantastico, grape, 2.20
+Revision
 
-//fantastico->
-//Product: apple, Price: 1.2
-//Product: grape, Price: 2.2
-//kaufland->
-//Product: banana, Price: 1.1
-//lidl->
-//Product: peach, Price: 1.2
-//Product: juice, Price: 2.3
+output:
+
+fantastico->
+Product: apple, Price: 1.2
+Product: grape, Price: 2.2
+kaufland->
+Product: banana, Price: 1.1
+lidl->
+Product: peach, Price: 1.2
+Product: juice, Price: 2.3
+
+ */
