a071ca9fd2cdcf376cd14d9d64f816efa1f8ff37
AtanasKaloyanov <atanas.kaloyanov@yahoo.com>
2024-06-13 00:26:59 +0300

    This is updated the solution of the problem "Largest 3 Numbers".

--- T03SetsAndMapsAdvanced/Lab/P09Largest3Numbers.java
+++ T03SetsAndMapsAdvanced/Lab/P09Largest3Numbers.java
@@ -1,28 +1,16 @@
 package T03SetsAndMapsAdvanced.Lab;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class P09Largest3Numbers {
     public static void main(String[] args) {
+        // Input reading and printing
         Scanner scanner = new Scanner(System.in);
 
-        List<Integer> numbers = Arrays.stream(scanner.nextLine().split(" "))
-                .map(e -> Integer.parseInt(e))
-                .sorted(
-                        (left, right) -> right.compareTo(left)
-                )
-                . collect(Collectors.toList());
-
-        for (int i = 0; i < 3; i++) {
-          if (i == numbers.size()) {
-              break;
-          }
-            System.out.print(numbers.get(i) + " ");
-        }
-
+        Arrays.stream(scanner.nextLine().split(" "))
+                .map(Integer::parseInt)
+                .sorted(Comparator.reverseOrder()).limit(3)
+                .forEach(number -> System.out.print(number + " "));
     }
 }
