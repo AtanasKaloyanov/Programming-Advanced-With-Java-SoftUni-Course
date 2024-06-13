507238f6ed98d480a3e3552c5fde13904ed2efaf
AtanasKaloyanov <atanas.kaloyanov@yahoo.com>
2024-06-13 17:09:25 +0300

    This is the updated solution of the problem "Sets of the elements".

--- T03SetsAndMapsAdvanced/Exercise/P02SetsOfElements.java
+++ T03SetsAndMapsAdvanced/Exercise/P02SetsOfElements.java
@@ -4,34 +4,31 @@ import java.util.*;
 
 public class P02SetsOfElements {
     public static void main(String[] args) {
+        // 1. Input reading
         Scanner scanner = new Scanner(System.in);
-
         String[] array = scanner.nextLine().split(" ");
-        int firstSetSize = Integer.parseInt(array[0]);
-        int secondSetSize = Integer.parseInt(array[1]);
 
-        Set<Integer> firstSet = new LinkedHashSet<>();
-        Set<Integer> secondSet = new LinkedHashSet<>();
+        int n1 = Integer.parseInt(array[0]);
+        int n2 = Integer.parseInt(array[1]);
 
-        for (int i = 0; i < firstSetSize; i++) {
-            int currentInt = Integer.parseInt(scanner.nextLine());
-            firstSet.add(currentInt);
+        // 2. Adding the elements to the both sets
+        Set<String> set1 = new LinkedHashSet<>();
+        for (int i = 0; i < n1; i++) {
+            String currentElement = scanner.nextLine();
+            set1.add(currentElement);
         }
 
-        for (int i = 0; i < secondSetSize; i++) {
-            int currentInt = Integer.parseInt(scanner.nextLine());
-            secondSet.add(currentInt);
+        // 3. If the set contains the current element => printing
+        Set<String> set2 = new LinkedHashSet<>();
+        for (int i = 0; i < n2; i++) {
+            String currentElement = scanner.nextLine();
+            set2.add(currentElement);
         }
 
-        Set<Integer> finalSet = new LinkedHashSet<>();
-
-        for (int currentNumber : firstSet) {
-            if (secondSet.contains(currentNumber)) {
-                finalSet.add(currentNumber);
-            }
-        }
-
-        finalSet.forEach(e -> System.out.print(e + " "));
+        // 4. Finding the section of the sets and printing
+        set1.retainAll(set2);
+        String result = set1.toString().replaceAll("[\\[\\],]", "");
+        System.out.println(result);
     }
 }
 
