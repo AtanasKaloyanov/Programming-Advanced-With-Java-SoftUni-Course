a94a3d0c9d72a359cc347d7b43883d6dc96c32b7
AtanasKaloyanov <atanas.kaloyanov@yahoo.com>
2024-06-13 15:21:02 +0300

    This is the solution of the problem "Academy Graduation".

--- T03SetsAndMapsAdvanced/Lab/P08AcademyGraduation.java
+++ T03SetsAndMapsAdvanced/Lab/P08AcademyGraduation.java
@@ -6,36 +6,43 @@ import java.util.stream.Collectors;
 
 public class P08AcademyGraduation {
     public static void main(String[] args) {
+        // 1. Input reading
         Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+        Map<String, Double> avgScoreByPerson = new TreeMap<>();
 
-        int numberStudents = Integer.parseInt(scanner.nextLine());
-        Map<String, List<Double>> map = new TreeMap<>();
-
-        for (int i = 1; i <= numberStudents; i++) {
-
-                String name = scanner.nextLine();
-                List<Double> grades = Arrays.stream(scanner.nextLine().split(" "))
-                        .map(e -> Double.parseDouble(e))
-                        .collect(Collectors.toList());
-
-                map.putIfAbsent(name, new ArrayList<>());
-
-                for (int k = 0; k < grades.size(); k++) {
-                    double currentGrade = grades.get(k);
-                    map.get(name).add(currentGrade);
-                }
-
+        // 2. Putting a name as a key into a map and as a value average grade .
+        // The average grade should be calculated
+        for (int i = 0; i < n; i++) {
+            String name = scanner.nextLine();
+            double avgGrade = getAvgGrade(scanner);
+            avgScoreByPerson.put(name, avgGrade);
         }
 
-        map.forEach( (key, value) -> {
+        // 3. Result printing
+        avgScoreByPerson.entrySet().stream()
+                .forEach( (entry) -> {
+                    String name = entry.getKey();
+                    double avgGrade = entry.getValue();
+                    DecimalFormat df = new DecimalFormat("0.#####################");
+                    String avgGradeString = df.format(avgGrade);
+                    System.out.printf("%s is graduated with %s\n", name, avgGradeString);
+                });
+    }
 
-            double gradesSum = 0;
-            for (int i = 0; i < value.size(); i++) {
-                gradesSum += value.get(i);
-            }
+    private static double getAvgGrade(Scanner scanner) {
+        List<Double> grades = readGrades(scanner);
+        double gradesSum = 0;
+        for (Double grade : grades) {
+            gradesSum += grade;
+        }
 
-            DecimalFormat df = new DecimalFormat("0.##################");
-            System.out.printf("%s is graduated with %s%n", key, df.format(gradesSum / value.size()));
-        });
+        return gradesSum / grades.size();
+    }
+
+    private static List<Double> readGrades(Scanner scanner) {
+        return Arrays.stream(scanner.nextLine().split(" "))
+                .map(Double::parseDouble)
+                .collect(Collectors.toList());
     }
 }
