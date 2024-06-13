3c52f2baa1bed83adc57c2062f123f1192f74a00
AtanasKaloyanov <atanas.kaloyanov@yahoo.com>
2024-06-13 16:46:13 +0300

    This is the updated solution of the problem "Unique Usernames".

--- T03SetsAndMapsAdvanced/Exercise/P01UniqueUsernames.java
+++ T03SetsAndMapsAdvanced/Exercise/P01UniqueUsernames.java
@@ -6,16 +6,20 @@ import java.util.Set;
 
 public class P01UniqueUsernames {
     public static void main(String[] args) {
+        // 1. Input reading
         Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
 
-        int number = Integer.parseInt(scanner.nextLine());
-        Set<String> usernames = new LinkedHashSet<>();
-
-        for (int i = 1; i <= number ; i++) {
-            String currentUsername = scanner.nextLine();
-            usernames.add(currentUsername);
+        // 2. Adding the elements to a Set
+        Set<String> uniqueElements = new LinkedHashSet<>();
+        for (int i = 0; i < n; i++) {
+            String currentText = scanner.nextLine();
+            uniqueElements.add(currentText);
         }
 
-        usernames.forEach(System.out::println);
+        // 3. Output printing
+        for (String element : uniqueElements) {
+            System.out.println(element);
+        }
     }
 }
