2aa71362138e9e702ed8911b099c14ead16f3183
AtanasKaloyanov <atanas.kaloyanov@yahoo.com>
2024-06-16 19:38:22 +0300

    This is the updated solution of the problem "Copy Bites".

--- T04StreamsFilesAndDirectories/Lab/P03CopyBytes.java
+++ T04StreamsFilesAndDirectories/Lab/P03CopyBytes.java
@@ -5,31 +5,45 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class P03CopyBytes {
-    public static void main(String[] args) {
+    // 1. Paths const initializing
+    private static String inputPath = "D:\\Programming\\Projects\\Programming Advanced\\src\\T04StreamsFilesAndDirectories\\Lab\\Files\\input.txt";
+    private static String outputPath = "D:\\Programming\\Projects\\Programming Advanced\\src\\T04StreamsFilesAndDirectories\\Lab\\Files\\03.CopyBytesOutput.txt";
 
-        String inputPath = "D:\\Programming\\SoftUni\\Programming Advanced with Java\\9. Streams, Files and Directories\\Files Tor the Tasks\\input.txt";
-        String outputPath = "D:\\Programming\\SoftUni\\Programming Advanced with Java\\9. Streams, Files and Directories\\Files Tor the Tasks\\output.txt";
+    public static void main(String[] args) throws IOException {
+        // 2. try-catch-finally algorithm for printing
+        FileInputStream fis = null;
+        FileOutputStream fos = null;
 
         try {
-            FileInputStream in = new FileInputStream(inputPath);
-            FileOutputStream out = new FileOutputStream(outputPath);
+            fis = new FileInputStream(inputPath);
+            fos = new FileOutputStream(outputPath);
 
-            int oneByte = in.read();
-
-            while (oneByte >= 0) {
-                if (oneByte == 32 || oneByte == 10) {
-                    out.write(oneByte);
+            int currentElement = fis.read();
+            while (currentElement != -1) {
+                // 2.1. If the currentElement is new line or space
+                // casting the element to char and writing it
+                if (currentElement == 10 || currentElement == 32) {
+                    char elementAsChar = (char) currentElement;
+                    fos.write(elementAsChar);
+                    // 2.2. In other case getting the element as string
+                    // then as bytes array
+                    // and  finally writing the bytes
                 } else {
-                    String oneByteToString = String.valueOf(oneByte);
-                    for (int i = 0; i < oneByteToString.length(); i++) {
-                        out.write(oneByteToString.charAt(i));
-                    }
+                    String numberAsText = String.valueOf(currentElement);
+                    byte[] bytes = numberAsText.getBytes();
+                    fos.write(bytes);
                 }
-                oneByte = in.read();
-
+                currentElement = fis.read();
             }
         } catch (IOException e) {
-            e.printStackTrace();
+            System.out.print(e.getMessage());
+        } finally {
+            if (fis != null) {
+                fis.close();
+            }
+            if (fos != null) {
+                fos.close();
+            }
         }
     }
 }
