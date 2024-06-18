e07b8fbf36f31a30df4b6e0dddf26b8d61a17507
AtanasKaloyanov <atanas.kaloyanov@yahoo.com>
2024-06-18 20:35:06 +0300

    This is the updated solution of the problem "Nested Folders".

--- T04StreamsFilesAndDirectories/Lab/P07ListFiles.java
+++ T04StreamsFilesAndDirectories/Lab/P07ListFiles.java
@@ -3,12 +3,14 @@ package T04StreamsFilesAndDirectories.Lab;
 import java.io.File;
 
 public class P07ListFiles {
+    private static String path = "D:\\Programming\\Projects\\Programming Advanced\\src\\T04StreamsFilesAndDirectories\\Lab\\Files\\Files-and-Streams";
     public static void main(String[] args) {
-
-        File file = new File("D:\\Programming\\SoftUni\\Programming Advanced with Java\\9. Streams, Files and Directories\\Files Tor the Tasks\\Files-and-Streams");
+        // 1. Creating a file object and checking if it exists and if it is directory
+        File file = new File(path);
 
         if (file.exists()) {
             if (file.isDirectory()) {
+                // 2. Creating an array, looping through every element and if it isn't directory printing
                 File[] files = file.listFiles();
                 for (File currentFile : files) {
                     if (!currentFile.isDirectory()) {
