ae7e5b9ae3a4720084bd5869f35bf6710ce761c5
AtanasKaloyanov <atanas.kaloyanov@yahoo.com>
2024-06-20 00:50:29 +0300

    This is the updated solution of the problem "Merge Two Files".

--- T04StreamsFilesAndDirectories/Exercise/P07MergeTwoFiles.java
+++ T04StreamsFilesAndDirectories/Exercise/P07MergeTwoFiles.java
@@ -9,46 +9,36 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class P07MergeTwoFiles {
+    private static String firstPath = "D:\\Programming\\Projects\\Programming Advanced\\src\\T04StreamsFilesAndDirectories\\Exercise\\04. Java-Advanced-Files-and-Streams-Exercises-Resources\\inputOne.txt";
+    private static String secondPath = "D:\\Programming\\Projects\\Programming Advanced\\src\\T04StreamsFilesAndDirectories\\Exercise\\04. Java-Advanced-Files-and-Streams-Exercises-Resources\\inputTwo.txt";
+    private static String printPath = "D:\\Programming\\Projects\\Programming Advanced\\src\\T04StreamsFilesAndDirectories\\Exercise\\04. Java-Advanced-Files-and-Streams-Exercises-Resources\\newOutput.txt";
+    private static List<Integer> list = new ArrayList<>();
+
     public static void main(String[] args) {
 
-        String firstPath = "D:\\Programming\\SoftUni\\Programming Advanced with Java\\10. Exercise - Streams, Files and Directories\\04. Java-Advanced-Files-and-Streams-Exercises-Resources\\inputOne.txt";
-        String secondPath = "D:\\Programming\\SoftUni\\Programming Advanced with Java\\10. Exercise - Streams, Files and Directories\\04. Java-Advanced-Files-and-Streams-Exercises-Resources\\inputTwo.txt";
-
         try {
+            // 1. Adding the information of the first file into a list
             FileReader fileReader = new FileReader(firstPath);
             BufferedReader bufferedReader = new BufferedReader(fileReader);
 
-            List<Integer> list = new ArrayList<>();
-
             String line = bufferedReader.readLine();
-            while (line != null) {
-                String[] array = line.split("");
-                int currentNumber = Integer.parseInt(array[0]);
-                list.add(currentNumber);
+            addingInfo(line, "", list, bufferedReader);
 
-                line = bufferedReader.readLine();
-            }
-
+            // 2. Adding the info of the second file into a list
             FileReader secondFileReader = new FileReader(secondPath);
             BufferedReader secondBufferedReader = new BufferedReader(secondFileReader);
 
             String secondLine = secondBufferedReader.readLine();
-            while (secondLine != null) {
-                String[] array = secondLine.split(" ");
-                int currentNumber = Integer.parseInt(array[0]);
-                list.add(currentNumber);
+            addingInfo(secondLine, " ", list, secondBufferedReader);
 
-                secondLine = secondBufferedReader.readLine();
-            }
-
-            String printPath = "D:\\Programming\\SoftUni\\Programming Advanced with Java\\10. Exercise - Streams, Files and Directories\\04. Java-Advanced-Files-and-Streams-Exercises-Resources\\newOutput.txt";
+            // 3. Sorting and writing
             FileWriter fileWriter = new FileWriter(printPath);
-
             PrintWriter printWriter = new PrintWriter(fileWriter);
 
-            list.stream().sorted( (firstElement, secondElement) -> {
-                return firstElement.compareTo(secondElement);
-            }).forEach(number -> printWriter.println(number));
+            list.stream().sorted((firstElement, secondElement) -> {
+                        return firstElement.compareTo(secondElement);
+                    })
+                    .forEach(number -> printWriter.println(number));
 
             printWriter.close();
 
@@ -56,6 +46,15 @@ public class P07MergeTwoFiles {
             e.printStackTrace();
         }
 
+    }
 
+    private static void addingInfo(String line, String regex, List<Integer> list, BufferedReader bufferedReader) throws IOException {
+        while (line != null) {
+            String[] array = line.split(regex);
+            int currentNumber = Integer.parseInt(array[0]);
+            list.add(currentNumber);
+
+            line = bufferedReader.readLine();
+        }
     }
 }
