8553dd35c5350f722214bc52f087e13b654b9984
AtanasKaloyanov <atanas.kaloyanov@yahoo.com>
2024-06-26 20:36:44 +0300

    This is the updated solution of the problem "Add Vat".

--- T05FunctionalProgramming/Lab/P04AddVat.java
+++ T05FunctionalProgramming/Lab/P04AddVat.java
@@ -2,16 +2,23 @@ package T05FunctionalProgramming.Lab;
 
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.UnaryOperator;
 
 public class P04AddVat {
     public static void main(String[] args) {
+        // 1. Creating 3 functions:
+        Function<String, Double> convertingStringIntoDouble = Double::parseDouble;
+        UnaryOperator<Double> adding20Percent = number -> number * 1.20;
+        Consumer<Double> printer = number -> System.out.printf("%.2f\n", number);
+
+        // 2. Reading the input than converting, adding 20% and printing via the functions:
         Scanner scanner = new Scanner(System.in);
-
         System.out.println("Prices with VAT:");
-
         Arrays.stream(scanner.nextLine().split(", "))
-                .map(element -> Double.parseDouble(element))
-                .map(element -> 1.2 * element)
-                .forEach(element -> System.out.printf("%.2f%n", element));
+                .map(convertingStringIntoDouble)
+                .map(adding20Percent)
+                .forEach(printer);
     }
 }
