f4d80137f746a5d46e1e51eb018f3a5971ff2f5a
AtanasKaloyanov <atanas.kaloyanov@yahoo.com>
2024-06-26 19:53:26 +0300

    This is the updated solution of the problem "Count Uppercase Words".

--- T05FunctionalProgramming/Lab/P03CountUppercaseWords.java
+++ T05FunctionalProgramming/Lab/P03CountUppercaseWords.java
@@ -8,15 +8,21 @@ import java.util.stream.Collectors;
 
 public class P03CountUppercaseWords {
     public static void main(String[] args) {
+        // 1. Input reading and filtering the words that begin with capital letter.
+        // The filtering is done with a Predicate
         Scanner scanner = new Scanner(System.in);
+        List<String> list = readList(scanner);
 
-        Predicate<String> predicate = element -> Character.isUpperCase(element.charAt(0));
-
-        List<String> list = Arrays.stream(scanner.nextLine().split(" "))
-                .filter(predicate)
-                .collect(Collectors.toList());
-
+        // 2. Output printing:
         System.out.println(list.size());
-        list.stream().forEach(element -> System.out.println(element));
+        list.forEach(System.out::println);
+    }
+
+    private static List<String> readList(Scanner scanner) {
+        Predicate<String> beginWithCapitalLetter = word -> Character.isUpperCase(word.charAt(0));
+
+        return Arrays.stream(scanner.nextLine().split(" "))
+                .filter(beginWithCapitalLetter)
+                .collect(Collectors.toList());
     }
 }
