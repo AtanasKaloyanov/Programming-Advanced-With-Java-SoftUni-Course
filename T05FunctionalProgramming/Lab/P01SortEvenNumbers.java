25960a84aefa8e69c9afb8dbb4338995edda4da2
AtanasKaloyanov <atanas.kaloyanov@yahoo.com>
2024-06-26 18:34:20 +0300

    This is the updated solution of the problem "Sort Even Numbers".

--- T05FunctionalProgramming/Lab/P01SortEvenNumbers.java
+++ T05FunctionalProgramming/Lab/P01SortEvenNumbers.java
@@ -1,28 +1,45 @@
 package T05FunctionalProgramming.Lab;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 
 public class P01SortEvenNumbers {
     public static void main(String[] args) {
+        // 1. Input reading
         Scanner scanner = new Scanner(System.in);
+        List<Integer> numbers = readList(scanner);
 
+        // 2. Removing the odd numbers via Predicate and printing
+        Predicate<Integer> oddNumbersCondition = x -> x % 2 != 0;
+        numbers.removeIf(oddNumbersCondition);
+        printing(numbers);
 
-        String[] array = scanner.nextLine().split(", ");
-        List<Integer> list = Arrays.stream(array)
-                .map(e -> Integer.parseInt(e))
-                .filter(e -> e % 2 == 0)
+        // 3. New line
+        System.out.println();
+
+        // 4. Sorting via Comparator and printing
+        Comparator<Integer> comparator = Integer::compareTo;
+        numbers.sort(comparator);
+        printing(numbers);
+    }
+
+    private static void printing(List<Integer> numbers) {
+        for (int i = 0; i < numbers.size(); i++) {
+            int currentNumber = numbers.get(i);
+            if (i == numbers.size() - 1) {
+                System.out.print(currentNumber);
+                return;
+            }
+
+            System.out.print(currentNumber + ", ");
+        }
+    }
+
+    private static List<Integer> readList(Scanner scanner) {
+        return Arrays.stream(scanner.nextLine().split(", "))
+                .map(Integer::parseInt)
                 .collect(Collectors.toList());
-
-        System.out.println(String.join(", ", list.toString().replaceAll("[\\[\\]]", "")));
-
-        Collections.sort(list);
-
-        System.out.println(String.join(", ", list.toString().replaceAll("[\\[\\]]", "")));
-
     }
 }
