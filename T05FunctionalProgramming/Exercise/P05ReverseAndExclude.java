22ad0041e1eebfecd3f0697cd9e6fc8ee32bdc86
AtanasKaloyanov <atanas.kaloyanov@yahoo.com>
2024-06-27 15:28:09 +0300

    This is the solution for the problem "Reverse and Exclude".

--- T05FunctionalProgramming/Exercise/P05ReverseAndExclude.java
+++ T05FunctionalProgramming/Exercise/P05ReverseAndExclude.java
@@ -4,18 +4,32 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class P05ReverseAndExclude {
     public static void main(String[] args) {
-
+        // 1. Input reading:
         Scanner scanner = new Scanner(System.in);
+        List<Integer> list = readList(scanner);
+        int divider = Integer.parseInt(scanner.nextLine());
 
-        List<Integer> list = Arrays.stream(scanner.nextLine().split(" ")).map((number -> Integer.parseInt(number))).collect(Collectors.toList());
-        int number = Integer.parseInt(scanner.nextLine());
-
+        // 2. Reversing:
         Collections.reverse(list);
-        list.removeIf(element -> element % number == 0);
-        list.stream().forEach(element -> System.out.print(element + " "));
+
+        // 3. Predicate and Consumer initialization:
+        Predicate<Integer> isDivisible = number -> number % divider == 0;
+        Consumer<Integer> printer = number -> System.out.print(number + " ");
+
+        // 3. Removing and printing via the predicate and the consumer:
+        list.removeIf(isDivisible);
+        list.forEach(printer);
+    }
+
+    private static List<Integer> readList(Scanner scanner) {
+        return Arrays.stream(scanner.nextLine().split(" "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
     }
 }
