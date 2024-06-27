86808679f9c79755095d6a06ca3a504bc3c20e11
AtanasKaloyanov <atanas.kaloyanov@yahoo.com>
2024-06-27 15:40:46 +0300

    This is the update solution of the problem "Predicate for names".

--- T05FunctionalProgramming/Exercise/P06PredicateForNames.java
+++ T05FunctionalProgramming/Exercise/P06PredicateForNames.java
@@ -3,19 +3,20 @@ package T05FunctionalProgramming.Exercise;
 
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.function.Consumer;
 import java.util.function.Predicate;
 
 public class P06PredicateForNames {
     public static void main(String[] args) {
+        // 1. Predicate and Consumer initialization:
         Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+        Predicate<String> hasLength = word -> word.length() <= n;
+        Consumer<String> printer = System.out::println;
 
-        int numberSymbols = Integer.parseInt(scanner.nextLine());
-        String[] array = scanner.nextLine().split(" ");
-
-        Predicate<Integer> predicate = symbols -> symbols <= numberSymbols;
-        
-        Arrays.stream(array).
-                filter(element -> predicate.test(element.length()))
-                .forEach(element -> System.out.println(element));
+        // 2. Numbers reading and printing:
+        Arrays.stream(scanner.nextLine().split(" "))
+                .filter(hasLength)
+                .forEach(printer);
     }
 }
