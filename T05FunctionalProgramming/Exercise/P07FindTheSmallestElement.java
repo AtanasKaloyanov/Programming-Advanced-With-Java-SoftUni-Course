011fbe35f11a39219aa4ddbde4b64c479acd7eeb
AtanasKaloyanov <atanas.kaloyanov@yahoo.com>
2024-06-27 15:57:45 +0300

    This is the updated solution of the problem "Find the smallest element".

--- T05FunctionalProgramming/Exercise/P07FindTheSmallestElement.java
+++ T05FunctionalProgramming/Exercise/P07FindTheSmallestElement.java
@@ -6,17 +6,24 @@ import java.util.stream.Collectors;
 
 public class P07FindTheSmallestElement {
     public static void main(String[] args) {
-
+        // 1. Input reading:
         Scanner scanner = new Scanner(System.in);
-        List<Integer> givenList = Arrays.stream(scanner.nextLine().split("\\s+"))
-                .map(number -> Integer.parseInt(number))
+        List<Integer> list = readList(scanner);
+
+        // 2. Finding the smallest element and creating a function that
+        // returns the rightmost index of this element:
+        int smallest = Collections.min(list);
+        Function<List<Integer>, Integer> smallerElementIndexOfList = l -> l.lastIndexOf(smallest);
+
+        // 2. Index of the smallest element printing:
+        int index = smallerElementIndexOfList.apply(list);
+        System.out.println(index);
+    }
+
+    private static List<Integer> readList(Scanner scanner) {
+        return Arrays.stream(scanner.nextLine().split(" "))
+                .map(Integer::parseInt)
                 .collect(Collectors.toList());
-
-        int smallestNumber = Collections.min(givenList);
-
-        Function<List<Integer>, Integer> function = list -> list.lastIndexOf(smallestNumber);
-        System.out.println(function.apply(givenList));
-
     }
 }
 
