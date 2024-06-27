a00ad03302f77ef43024ca36a23ab0d1b4f843d5
AtanasKaloyanov <atanas.kaloyanov@yahoo.com>
2024-06-27 18:10:53 +0300

    This is the updated solution of the problem "Custom Comparator".

--- T05FunctionalProgramming/Exercise/P08CustomComparator.java
+++ T05FunctionalProgramming/Exercise/P08CustomComparator.java
@@ -1,21 +1,35 @@
 package T05FunctionalProgramming.Exercise;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 
 public class P08CustomComparator {
     public static void main(String[] args) {
-
+        // 1. Input reading:
         Scanner scanner = new Scanner(System.in);
+        Integer[] numbers = readArray(scanner);
 
-        List<Integer> list = Arrays.stream(scanner.nextLine().split(" "))
-                .map(element -> Integer.parseInt(element))
-                .sorted( (firstNumber, secondNumber) -> firstNumber.compareTo(secondNumber))
-                .collect(Collectors.toList());
+        // 2. Comparator implementation:
+        Comparator<Integer> comp = (num1, num2) -> {
+            if (num1 % 2 == 0 && num2 % 2 != 0) {
+                return -1;
+            } else if (num1 % 2 != 0 && num2 % 2 == 0) {
+                return 1;
+            }
+            return num1.compareTo(num2);
+        };
 
-        list.stream().filter(element -> element % 2 == 0).forEach(element -> System.out.print(element + " "));
-        list.stream().filter(element -> element % 2 != 0).forEach(element -> System.out.print(element + " "));
+        // 3. Passing the array and the comparator in the sort function and printing:
+        Arrays.sort(numbers, comp);
+        System.out.println(Arrays.toString(numbers).replaceAll("[\\[\\],]", ""));
+    }
+
+    private static Integer[] readArray(Scanner scanner) {
+        return Arrays.stream(scanner.nextLine().split(" "))
+                .map(Integer::parseInt)
+                .toArray(Integer[]::new);
     }
 }
