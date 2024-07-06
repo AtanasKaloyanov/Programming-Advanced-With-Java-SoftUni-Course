c78e86cf993cf86bbbcf3a24ac8f3087a5233a14
AtanasKaloyanov <atanas.kaloyanov@yahoo.com>
2024-07-06 21:03:26 +0300

    This is the updated solution of the problem "Generic Swap Method Strings".

--- T08Generics/Exercise/P03GenericSwapMethodStrings/Main.java
+++ T08Generics/Exercise/P03GenericSwapMethodStrings/Main.java
@@ -4,21 +4,24 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
+        // 1. Input reading:
         Scanner scanner = new Scanner(System.in);
-        int number = Integer.parseInt(scanner.nextLine());
-        Box<String> box = new Box<>();
+        int n = Integer.parseInt(scanner.nextLine());
 
-        for (int i = 1; i <= number ; i++) {
-            String element = scanner.nextLine();
-            box.add(element);
+        // 2. Box initialization and adding every element:
+        Box<String> box = new Box<>();
+        for (int i = 0; i < n; i++) {
+            String current = scanner.nextLine();
+            box.addElement(current);
         }
 
-        int firstElement = scanner.nextInt();
-        int secondElement = scanner.nextInt();
+        // 3. Indices reading and swapping elements via swap method:
+        String[] indices = scanner.nextLine().split(" ");
+        int index1 = Integer.parseInt(indices[0]);
+        int index2 = Integer.parseInt(indices[1]);
+        box.swap(index1, index2);
 
-        box.swap(firstElement, secondElement);
-
+        // 4. Output printing:
         System.out.println(box);
     }
 }
