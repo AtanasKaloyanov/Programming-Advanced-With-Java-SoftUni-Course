78fc732e3cb59335a82cae067c824479887067db
AtanasKaloyanov <atanas.kaloyanov@yahoo.com>
2024-07-06 17:42:55 +0300

    This is the updated solution of the problem "Generic Array Creator".

--- T08Generics/Lab/P02GenericArrayCreator/ArrayCreator.java
+++ T08Generics/Lab/P02GenericArrayCreator/ArrayCreator.java
@@ -1,26 +1,18 @@
 package T08Generics.Lab.P02GenericArrayCreator;
 
 import java.lang.reflect.Array;
+import java.util.Arrays;
 
 public class ArrayCreator {
-
-          public static <T> T[] create(int length, T item) {
-              T[] array = (T[]) Array.newInstance(item.getClass(), length);
-
-              for (int i = 0; i < length; i++) {
-                  array[i] = item;
-              }
-
-              return array;
+    public static<T> T[] create(int length, T element) {
+        T[] array = (T[]) Array.newInstance(element.getClass(), length);
+        Arrays.fill(array, element);
+        return array;
     }
 
-        public static <T> T[] create(Class<T> clazz, int length, T item) {
-            T[] array = (T[]) Array.newInstance(clazz, length);
-
-            for (int i = 0; i < array.length; i++) {
-                array[i] = item;
-            }
-
-            return array;
-        }
+    public static<T> T[] create(Class<T> clazz, int length, T element) {
+        T[] array = (T[]) Array.newInstance(clazz, length);
+        Arrays.fill(array, element);
+        return array;
+    }
 }
--- T08Generics/Lab/P02GenericArrayCreator/Main.java
+++ T08Generics/Lab/P02GenericArrayCreator/Main.java
@@ -2,12 +2,26 @@ package T08Generics.Lab.P02GenericArrayCreator;
 
 public class Main {
     public static void main(String[] args) {
-          ArrayCreator arrayCreator = new ArrayCreator();
+        // 1. Creating an array with the first method:
+        // create(int length, E element)
+        // and printing its elements:
 
-          ArrayCreator.create(5, "Pesho");
-          ArrayCreator.create(String.class, 5, "Pesho");
+        String[] array = ArrayCreator.create(5, "A");
+        printing(array);
 
+        // 2. New line between the arrays printing:
+        System.out.println();
 
+        // 2. Creating a second array via the second method:
+        // create(Class<T> clazz, int length, T element)
+        // and printing it:
+        String[] array2 = ArrayCreator.create(String.class, 7, "B");
+        printing(array2);
+    }
 
+    public static void printing(String[] array) {
+        for (String element : array) {
+            System.out.print(element + " ");
+        }
     }
 }
