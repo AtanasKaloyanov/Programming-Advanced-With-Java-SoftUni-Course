fad84a13300d4fadfae126468d17b0107a60c888
AtanasKaloyanov <atanas.kaloyanov@yahoo.com>
2024-07-06 21:46:07 +0300

    This is the updated solution of the problem "Generic Count Method Doubles".

--- T08Generics/Exercise/P06GenericCountMethodDoubles/Box.java
+++ T08Generics/Exercise/P06GenericCountMethodDoubles/Box.java
@@ -4,24 +4,41 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Box<T extends Comparable> {
-    private List<T> list;
-
-    public Box() {
-        this.list = new ArrayList<>();
+    private List<T> elements = new ArrayList<>();
+    public void addElement(T element) {
+        elements.add(element);
     }
 
-    public void add(T element) {
-        list.add(element);
+    public void swap(int index1, int index2) {
+        T element1 = this.elements.get(index1);
+        T element2 = this.elements.get(index2);
+        this.elements.set(index1, element2);
+        this.elements.set(index2, element1);
     }
 
-    public int countingBetterElements(T givenElement) {
+    public int greaterElementsCount(T compareElement) {
         int counter = 0;
-        for (T currentElement : this.list) {
-            if (currentElement.compareTo(givenElement) > 0) {
+        for (T element : this.elements) {
+            int comparator = element.compareTo(compareElement);
+            if (comparator > 0) {
                 counter++;
             }
         }
+
         return counter;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (T element : elements) {
+            String classInfo = element.getClass().getName();
+            sb.append(classInfo)
+                    .append(": ")
+                    .append(element)
+                    .append("\n");
+        }
+        return sb.toString();
     }
 }
 
--- T08Generics/Exercise/P06GenericCountMethodDoubles/Main.java
+++ T08Generics/Exercise/P06GenericCountMethodDoubles/Main.java
@@ -1,22 +1,27 @@
 package T08Generics.Exercise.P06GenericCountMethodDoubles;
 
-import T08Generics.Exercise.P05GenericCountMethodStrings.Box;
-
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        // 1. Input reading:
         Scanner scanner = new Scanner(System.in);
-        int number = Integer.parseInt(scanner.nextLine());
+        int n = Integer.parseInt(scanner.nextLine());
 
+        // 2. Box initialization and elements reading and adding to the box:
         Box<Double> box = new Box<>();
-        for (int i = 1; i <= number; i++) {
-            double currentNumber = Double.parseDouble(scanner.nextLine());
-            box.add(currentNumber);
+        for (int i = 0; i < n; i++) {
+            Double element = Double.parseDouble(scanner.nextLine());
+            box.addElement(element);
         }
 
-        double numberForCompare = Double.parseDouble(scanner.nextLine());
-        System.out.println(box.countingBetterElements(numberForCompare));
+        // 3. CompareElement reading and becoming the count of the better element
+        // via the method greaterElementsCount()
+        Double compareElement = Double.parseDouble(scanner.nextLine());
+        int betterElementsCount = box.greaterElementsCount(compareElement);
+
+        // 4. Output printing:
+        System.out.println(betterElementsCount);
     }
 }
 
